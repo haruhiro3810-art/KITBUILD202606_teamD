e68866080c010c9a5acef25d172647d4ec8966d3
nikujaga <n.takuto.1222@gmail.com>
2026-06-16 18:13:14 +0900

    縦スクロール検証2

--- src/scenes/StageScene.ts
+++ src/scenes/StageScene.ts
@@ -9,18 +9,18 @@ export default class StageScene extends Phaser.Scene {
     }
 create() {
     console.log('StageScene')
-    this.player = this.add.rectangle(100, 300, 40, 40, 0x00ff00);
+    this.player = this.add.rectangle(100, 500, 40, 40, 0x00ff00);
     //this.physics.add.existing(this.player);
 
     this.cursors = this.input.keyboard!.createCursorKeys();
   }
 
 update(){
-    if (this.cursors.right?.isDown){
-        this.player.x += 3;
+    if (this.cursors.right?.isDown && this.player.x + 5 <= 780){
+        this.player.x += 5;
     }
-    if (this.cursors.left?.isDown){
-        this.player.x -= 3;
+    if (this.cursors.left?.isDown && this.player.x -5 >= 20){
+        this.player.x -= 5;
     }
   }
  }
