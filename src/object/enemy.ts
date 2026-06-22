a3a34df086656481daa841337a87c76973392040
kksms13 <haruhiro3810@gmail.com>
2026-06-22 10:58:28 +0900

    ライフに関する関数を作った

--- src/object/enemy.ts
+++ src/object/enemy.ts
@@ -1,10 +1,12 @@
 import Phaser from 'phaser';
 
-//  敵を作るための仕組みの土台
+// 👾 敵（エネミー）を作るための仕組みの土台
 export default class Enemy {
     constructor() {
+        // TODO: ここに敵の初期設定をあとで書く
     }
 
     update() {
+        // TODO: ここに敵の動きをあとで書く
     }
 }
